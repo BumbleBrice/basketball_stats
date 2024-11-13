16146def5403a4e50c81301b94bf840ea64fdf3c
Bumble <collillieux.brice@gmail.com>
2024-11-13 02:23:14 +0100

    reconfiguration du server.js, ajout des erreurs non capturées, dotEnv appeler en premier

--- server.js
+++ server.js
@@ -4,11 +4,23 @@ const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
+}).on('error', (err) => {
+  console.error('Erreur de démarrage du serveur:', err);
+  process.exit(1);
 });
 
 // Gestion de l'arrêt propre du serveur
 process.on('SIGTERM', () => {
   console.info('SIGTERM signal reçu. Arrêt du serveur...');
+  server.close(() => {
+    console.log('Serveur arrêté');
+    process.exit(0);
+  });
+});
+
+// Ajout de la gestion de SIGINT (Ctrl+C)
+process.on('SIGINT', () => {
+  console.info('SIGINT signal reçu. Arrêt du serveur...');
   server.close(() => {
     console.log('Serveur arrêté');
     process.exit(0);
